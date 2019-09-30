a3d475541e29f0ff461ae3b430c44f7e75c40ebe
ilhamva <ilhamva@gmail.com>
2019-09-30 18:12:15 -0400

    observer pattern es6

---
+++ observer pattern(with es6)/app.js
@@ -0,0 +1,62 @@
+class EventObserver{
+    constructor(){
+        this.observers = [];
+    }
+
+    subscribe(fn){
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`)
+    };
+
+    unsubscribe(fn){
+        this.observers = this.observers.filter(function(item){
+            if(item !== fn){
+                return item;
+            }
+        });
+        console.log(`You are now unsubscribed from ${fn.name}`);
+    };
+
+    fire(){
+        this.observers.forEach(function (item) {
+            item.call();
+        })
+    }
+
+};
+
+const click = new EventObserver();
+
+document.querySelector('.sub-ms').addEventListener('click', function(){
+    click.subscribe(getCurrentMillisecond);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function(){
+    click.unsubscribe(getCurrentMillisecond);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function(){
+    click.subscribe(getCurrentSecond);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function(){
+    click.unsubscribe(getCurrentSecond);
+});
+
+document.querySelector('.fire').addEventListener('click', function(){
+    click.fire();
+});
+
+const getCurrentMillisecond = function () {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`)
+}
+
+const getCurrentSecond = function () {
+    console.log(`Current Milliseconds: ${new Date().getSeconds()}`)
+}
+
+
+
+
+
+
